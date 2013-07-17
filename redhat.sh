a747fb8809122dad2c63b63cc8a8f6dfa0b65754
James Kyle <james@jameskyle.org>
2013-07-17 12:02:49 -0700

    change to github repo url instead of gist

--- redhat.sh
+++ redhat.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-SHA="f804697ee81f057ea69716e2fe7d35dd4f6db97e"
 rpm -ivh http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm
 yum install puppet -y
 
-curl "https://gist.github.com/jameskyle/6017819/raw/${SHA}/setup.pp" -o /tmp/setup.pp
+curl "https://raw.github.com/jameskyle/setup/master/setup.pp" -o /tmp/setup.pp
 
 puppet apply /tmp/setup.pp
 
--- ubuntu.sh
+++ ubuntu.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 
-SHA="f804697ee81f057ea69716e2fe7d35dd4f6db97e"
 wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
 dpkg -i puppetlabs-release-precise.deb
 apt-get update
 
 apt-get install puppet -y
 
-curl "https://gist.github.com/jameskyle/6017819/raw/${SHA}/setup.pp" -o /tmp/setup.pp
+curl "https://raw.github.com/jameskyle/setup/master/setup.pp" -o /tmp/setup.pp
 
 puppet apply /tmp/setup.pp
 
