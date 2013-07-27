8d78a342db20b11f681fd8af9e0612570b6a5080
James Kyle <james@jameskyle.org>
2013-07-27 13:21:54 -0700

    Added script to setup redhat based os's for vagrant boxing

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@
 wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
 dpkg -i puppetlabs-release-precise.deb
 apt-get update
-
+apt-get dist-upgrade -y
 apt-get install puppet -y
 
 curl "https://raw.github.com/jameskyle/setup/master/setup.pp" -o /tmp/setup.pp
