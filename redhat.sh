3823b6b0c271e160156e44fbd85636a124621f5b
James Kyle <james@jameskyle.org>
2014-05-16 11:15:11 -0400

    Github raw url changed.

--- redhat.sh
+++ redhat.sh
@@ -3,7 +3,7 @@
 rpm -ivh http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm
 yum install puppet -y
 
-curl "https://raw.github.com/jameskyle/setup/master/setup.pp" -o /tmp/setup.pp
+curl "https://raw.githubusercontent.com/jameskyle/setup/master/setup.pp" -o /tmp/setup.pp
 
 puppet apply /tmp/setup.pp
 
--- ubuntu.sh
+++ ubuntu.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -ex
 
-apt-get update
-apt-get install wget
 codename=`lsb_release -c -s`
-wget http://apt.puppetlabs.com/puppetlabs-release-${codename}.deb
+
+apt-get autoremove --purge -y
+apt-get update
+curl -O http://apt.puppetlabs.com/puppetlabs-release-${codename}.deb
 dpkg -i puppetlabs-release-${codename}.deb
 apt-get update
 apt-get dist-upgrade -y
 apt-get install puppet -y
 
-curl "https://raw.github.com/jameskyle/setup/master/setup.pp" -o /tmp/setup.pp
+curl "https://raw.githubusercontent.com/jameskyle/setup/master/setup.pp" -o /tmp/setup.pp
 
 puppet apply /tmp/setup.pp
 
